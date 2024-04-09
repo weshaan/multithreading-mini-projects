5361112b6cc0c064814114cff51cd4afa1ffdd55
Eshaan Walia <118920744+weshaan@users.noreply.github.com>
2024-04-09 16:29:08 +0530

    p1.2

---
+++ P1/one_by_one.py
@@ -0,0 +1,38 @@
+# import os
+# os.getcwd()
+
+import random as r, string as s, shutil, os, time
+
+inputDIR='InputDirectory'
+totalFiles=20
+fileSize=20
+
+try:
+    shutil.rmtree("./%s/"%(inputDIR))
+    os.mkdir(inputDIR)
+except:
+    os.mkdir(inputDIR)
+
+def task(filename):
+    itr=512
+    filename="./%s/%s"%(inputDIR,filename)
+    while True:
+        fp=open(filename,"a")
+        for j in range(itr):
+            for k in range(itr):
+                string=" ".join(r.sample(s.ascii_letters,50))
+                fp.write(string + "\n" + string + "\n" + string + "\n" + string + "\n" + string + "\n")
+        fp.close()
+        if os.path.getsize(filename)>=fileSize:
+            break
+    return None
+
+startTime=time.time()
+print("Execution has started!")
+for i in range(totalFiles):
+    filename="test%d.txt"%(i+1)
+    task(filename)
+    if i%5==0:
+        print("File %s created!"%(filename))
+print("\nTotal time taken is %f seconds"%(round(time.time()-startTime,4)))
+print("Ending execution.")
